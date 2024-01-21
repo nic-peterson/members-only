e9e4114c38691919443598b85f81599a72e0e94a
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-21 13:27:36 -0800

    Updated the User model to validate the presence and uniqueness of the username. Updated the ApplicationController to permit the username parameter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,5 +3,12 @@ require "application_responder"
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
   respond_to :html
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
