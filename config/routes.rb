9d4a3cf896fcb6ebbc2b360be513d5f84234c267
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-21 12:47:03 -0800

    Changed routes->root from  to .

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :posts, only: [:new, :create, :index]
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/index'
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,5 +10,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root to: "home#index"
+  root to: "posts#index"
 end
