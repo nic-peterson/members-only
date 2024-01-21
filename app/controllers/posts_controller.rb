5041bc7f4f315945ec01d2cd8b7722b1acc3b527
Nic Peterson <23559376+nic-peterson@users.noreply.github.com>
2024-01-21 12:53:32 -0800

    Defined Post Controller methods #new, #create, #index, and private #post_params.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,26 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def new
+    @post = current_user.posts.build
   end
 
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save!
+      redirect_to posts_path, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
   end
 
   def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
